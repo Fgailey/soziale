2cadd12a63f0e44f42cf96784f21f626bb49092a
Foster Gailey <foster@gaileyhome.com>
2020-01-20 14:55:29 -0500

    added routes

---
+++ client/src/components/navbar/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+        <span>Nav Page </span>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ client/src/pages/Chat.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Chat = () => {
+  return (
+    <div>
+        <span>Chat Page </span>
+    </div>
+  );
+}
+
+export default Chat;
