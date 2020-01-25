4790778a5855208aebb3d2a478c80b25bfa90f0c
Bijan Dayani <bdayani518@gmail.com>
2020-01-24 21:12:30 -0500

    local mongo connection

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// connect to mongo database
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log("It's in ;)"))
+.catch(err => console.log(err));
+
 // Define API routes here
 
 // Send every other request to the React app
