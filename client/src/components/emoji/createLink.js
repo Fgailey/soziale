4c57943e657ece0f252be61ba6e16d74de580678
Ali Rahimlou <Rahimlou91@gmail.com>
2020-01-26 13:16:55 -0500

    picker

--- client/src/components/emoji/createLink.js
+++
@@ -1,8 +0,0 @@
-export default (str, match, method) => {
-  const link = document.createElement('a')
-  link.innerHTML = match.emoji
-  link.setAttribute('title', match.name)
-  link.setAttribute('class', 'emoji')
-  link.addEventListener('click', () => method(str, match.emoji))
-  return link
-}
