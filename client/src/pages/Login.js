afd36fa40a426cf5fd0ec4eaddf5ad4f705d86da
Scott Ladd <scottjr101@gmail.com>
2020-01-27 04:30:01 -0500

    added login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,111 @@
-import React from "react";
+import React, { Fragment, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../actions/Auth';
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    login(email, password);
+  };
+
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
+
   return (
-    <div>
-        <span>Login Page </span>
-    </div>
+    <Fragment>
+      <div className='container mb-5 mt-5'>
+        <div className='card'>
+          {/* Default form login */}
+          <form
+            className='text-center border border-light p-5'
+            action='#!'
+            onSubmit={e => onSubmit(e)}
+          >
+            <p className='h4 mb-4'>Sign in</p>
+            {/* Email */}
+            <input
+              type='email'
+              id='defaultLoginFormEmail'
+              className='form-control mb-4'
+              placeholder='E-mail'
+              name='email'
+              value={email}
+              onChange={e => onChange(e)}
+              required
+            />
+            {/* Password */}
+            <input
+              type='password'
+              id='defaultLoginFormPassword'
+              className='form-control mb-4'
+              placeholder='Password'
+              name='password'
+              value={password}
+              onChange={e => onChange(e)}
+              minLength='6'
+            />
+            <div className='d-flex justify-content-around'>
+              <div>
+                {/* Remember me */}
+                <div className='custom-control custom-checkbox'>
+                  <input
+                    type='checkbox'
+                    className='custom-control-input'
+                    id='defaultLoginFormRemember'
+                  />
+                  <label
+                    className='custom-control-label'
+                    htmlFor='defaultLoginFormRemember'
+                  >
+                    Remember me
+                  </label>
+                </div>
+              </div>
+              <div>
+                {/* Forgot password */}
+                <a href='/'>Forgot password?</a>
+              </div>
+            </div>
+            {/* Sign in button */}
+            <button className='btn btn-info btn-block my-4' type='submit'>
+              Sign in
+            </button>
+            {/* Register */}
+            <p>
+              Not a member?
+              <Link to='/register'>Register</Link>
+            </p>
+          </form>
+          {/* Default form login */}
+        </div>
+      </div>
+    </Fragment>
   );
-}
+};
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(
+  mapStateToProps,
+  { login }
+)(Login);
