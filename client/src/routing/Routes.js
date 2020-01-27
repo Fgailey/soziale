8f089c96a61905504b284e6e65d27b9b983ece4c
Scott Ladd <scottjr101@gmail.com>
2020-01-27 04:32:49 -0500

    formatting

--- client/src/routing/Routes.js
+++ client/src/routing/Routes.js
@@ -1,24 +1,24 @@
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Login from "../pages/Login"
-import Register from "../pages/Register"
-import Dashboard from "../pages/Dashboard"
-import Chat from "../pages/Chat"
-import Profile from "../pages/Profile"
-import NoMatch from "../pages/NoMatch"
-// import Alert from '../layout/Alert';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Dashboard from '../pages/Dashboard';
+import Chat from '../pages/Chat';
+import Profile from '../pages/Profile';
+import NoMatch from '../pages/NoMatch';
+// import PrivateRoute from './PrivateRoute';
 
 const Routes = () => (
-      <Fragment>
-      <Switch>
-      <Route exact path="/dashboard" component={Dashboard}/>
-          <Route exact path="/chat" component={Chat}/>
-          <Route exact path="/profile" component={Profile}/>
-          <Route exact path="/login" component={Login}/>
-          <Route exact path="/register" component={Register}/>
-          <Route component={NoMatch} />
-      </Switch>
-      </Fragment>
+  <Fragment>
+    <Switch>
+      <Route exact path='/dashboard' component={Dashboard} />
+      <Route exact path='/chat' component={Chat} />
+      <Route exact path='/profile' component={Profile} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+      <Route component={NoMatch} />
+    </Switch>
+  </Fragment>
 );
 
 export default Routes;
