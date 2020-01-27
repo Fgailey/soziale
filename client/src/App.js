87ada881a9c712bb7ea905ff428794a24fd196f6
Scott Ladd <scottjr101@gmail.com>
2020-01-27 04:27:11 -0500

    added footer and redux code

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,39 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Landing from "./pages/Landing"
-import Nav from "./components/navbar/Nav"
-import Routes from './routing/Routes'
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Landing from './pages/Landing';
+import Nav from './components/navbar/Nav';
+import Footer from './components/footer/Footer';
+import Routes from './routing/Routes';
 import './App.css';
 
-const App = () => (
-    <Router>
-      <Fragment>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route component={Routes} />
-        </Switch>
-      </Fragment>
-    </Router>
-); 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/Auth';
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+          <Footer />
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 export default App;
-
-
