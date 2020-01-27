9ee626d30c464e936e0bb06ea08491e9e48f0e84
Scott Ladd <scottjr101@gmail.com>
2020-01-27 04:31:31 -0500

    changed over href to Link to

--- client/src/components/navbar/Nav.js
+++ client/src/components/navbar/Nav.js
@@ -1,26 +1,25 @@
 import React from "react";
-import './Styles.css';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark sticky-top font-weight-bold blue-gradient">
-        <a className="navbar-brand" href="/">Navbar</a>
+        <Link className="navbar-brand" to="/">Navbar</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <a className="nav-item nav-link active" href="/">Home <span className="sr-only">current</span></a>
-            <a className="nav-item nav-link" href="/chat">Chat</a>
-            <a className="nav-item nav-link" href="/profile">Profile</a>
-            <a className="nav-item nav-link" href="/dashboard">Dashboard</a>
-            <a className="nav-item nav-link" href="/login">Login</a>
-            <a className="nav-item nav-link" href="/register">Register</a>
+            <Link className="nav-item nav-link active" to="/">Home <span className="sr-only">current</span></Link>
+            <Link className="nav-item nav-link" to="/chat">Chat</Link>
+            <Link className="nav-item nav-link" to="/profile">Profile</Link>
+            <Link className="nav-item nav-link" to="/dashboard">Dashboard</Link>
+            <Link className="nav-item nav-link" to="/login">Login</Link>
+            <Link className="nav-item nav-link" to="/register">Register</Link>
           </div>
         </div>
       </nav>
-
     </div>
   );
 }
