0af102cef832a6a9307bed2bf513c14a15f6665f
Scott Ladd <scottjr101@gmail.com>
2020-01-29 04:21:52 -0500

    added placeholder styling

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,11 +1,85 @@
-import React from "react";
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
-    <div>
-        <span>Dashboard Page </span>
-    </div>
+    <Fragment>
+      <div className='lightWool py-5'>
+        <div className='container'>
+          <div className='card pl-5 pr-5 py-5'>
+            <div className='row mx-1'>
+              <div className='col-md-6 mb-4'>
+                {/* Card */}
+                <div
+                  className='card card-image'
+                  style={{
+                    backgroundImage:
+                      'url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg)'
+                  }}
+                >
+                  {/* Content */}
+                  <div className='text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4'>
+                    <div>
+                      <h5 className='pink-text'>
+                        <i className='fas fa-chart-pie' /> Marketing
+                      </h5>
+                      <h3 className='card-title pt-2'>
+                        <strong>This is the card title</strong>
+                      </h3>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Repellat fugiat, laboriosam, voluptatem, optio
+                        vero odio nam sit officia accusamus minus error nisi
+                        architecto nulla ipsum dignissimos. Odit sed qui,
+                        dolorum!.
+                      </p>
+                      <Link to='#!' className='btn btn-pink'>
+                        <i className='fas fa-clone left' /> View project
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+                {/* Card */}
+              </div>
+              <div className='col-md-6 mb-4'>
+                {/* Card */}
+                <div
+                  className='card card-image'
+                  style={{
+                    backgroundImage:
+                      'url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg)'
+                  }}
+                >
+                  {/* Content */}
+                  <div className='text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4'>
+                    <div>
+                      <h5 className='pink-text'>
+                        <i className='fas fa-chart-pie' /> Marketing
+                      </h5>
+                      <h3 className='card-title pt-2'>
+                        <strong>This is the card title</strong>
+                      </h3>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Repellat fugiat, laboriosam, voluptatem, optio
+                        vero odio nam sit officia accusamus minus error nisi
+                        architecto nulla ipsum dignissimos. Odit sed qui,
+                        dolorum!.
+                      </p>
+                      <Link to='#!' className='btn btn-pink'>
+                        <i className='fas fa-clone left' /> View project
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+                {/* Card */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
-}
+};
 
 export default Dashboard;
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,143 @@
-import React from "react";
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
   return (
-    <div>
-        <span>Profile Page </span>
-    </div>
+    <Fragment>
+      <div className='lightWool py-5'>
+        <div className='container'>
+          <div className='card p-5'>
+            <div className='row row-cols-1 row-cols-md-2'>
+              <div className='col mb-4'>
+                {/* Card */}
+                <div className='card'>
+                  {/*Card image*/}
+                  <div className='view overlay'>
+                    <img
+                      className='card-img-top'
+                      src='https://mdbootstrap.com/img/Photos/Others/images/16.jpg'
+                      alt='Card cap'
+                    />
+                    <Link to='#!'>
+                      <div className='mask rgba-white-slight' />
+                    </Link>
+                  </div>
+                  {/*Card content*/}
+                  <div className='card-body'>
+                    {/*Title*/}
+                    <h4 className='card-title'>Card title</h4>
+                    {/*Text*/}
+                    <p className='card-text'>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+                    <button type='button' className='btn btn-light-blue btn-md'>
+                      Read more
+                    </button>
+                  </div>
+                </div>
+                {/* Card */}
+              </div>
+              <div className='col mb-4'>
+                {/* Card */}
+                <div className='card'>
+                  {/*Card image*/}
+                  <div className='view overlay'>
+                    <img
+                      className='card-img-top'
+                      src='https://mdbootstrap.com/img/Photos/Others/images/16.jpg'
+                      alt='Card cap'
+                    />
+                    <Link to='#!'>
+                      <div className='mask rgba-white-slight' />
+                    </Link>
+                  </div>
+                  {/*Card content*/}
+                  <div className='card-body'>
+                    {/*Title*/}
+                    <h4 className='card-title'>Card title</h4>
+                    {/*Text*/}
+                    <p className='card-text'>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+                    <button type='button' className='btn btn-light-blue btn-md'>
+                      Read more
+                    </button>
+                  </div>
+                </div>
+                {/* Card */}
+              </div>
+              <div className='col mb-4'>
+                {/* Card */}
+                <div className='card'>
+                  {/*Card image*/}
+                  <div className='view overlay'>
+                    <img
+                      className='card-img-top'
+                      src='https://mdbootstrap.com/img/Photos/Others/images/16.jpg'
+                      alt='Card cap'
+                    />
+                    <Link to='#!'>
+                      <div className='mask rgba-white-slight' />
+                    </Link>
+                  </div>
+                  {/*Card content*/}
+                  <div className='card-body'>
+                    {/*Title*/}
+                    <h4 className='card-title'>Card title</h4>
+                    {/*Text*/}
+                    <p className='card-text'>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+                    <button type='button' className='btn btn-light-blue btn-md'>
+                      Read more
+                    </button>
+                  </div>
+                </div>
+                {/* Card */}
+              </div>
+              <div className='col mb-4'>
+                {/* Card */}
+                <div className='card'>
+                  {/*Card image*/}
+                  <div className='view overlay'>
+                    <img
+                      className='card-img-top'
+                      src='https://mdbootstrap.com/img/Photos/Others/images/16.jpg'
+                      alt='Card cap'
+                    />
+                    <Link to='#!'>
+                      <div className='mask rgba-white-slight' />
+                    </Link>
+                  </div>
+                  {/*Card content*/}
+                  <div className='card-body'>
+                    {/*Title*/}
+                    <h4 className='card-title'>Card title</h4>
+                    {/*Text*/}
+                    <p className='card-text'>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+                    <button type='button' className='btn btn-light-blue btn-md'>
+                      Read more
+                    </button>
+                  </div>
+                </div>
+                {/* Card */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
-}
+};
 
 export default Profile;
