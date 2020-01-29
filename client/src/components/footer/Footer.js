d85df333f543d78eae71387aab7547ad48331b1c
Scott Ladd <scottjr101@gmail.com>
2020-01-29 04:21:19 -0500

    replaced footer with react hooks w/ mdb

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,53 +1,45 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from 'mdbreact';
 import { Link } from 'react-router-dom';
 
-export const Footer = () => {
+const Footer = () => {
   return (
-    <Fragment>
-      {/* Footer */}
-      <footer className='page-footer font-small teal pt-4 sticky-bottom'>
-        {/* Footer Text */}
-        <div className='container-fluid text-center text-md-left'>
-          {/* Grid row */}
-          <div className='row'>
-            {/* Grid column */}
-            <div className='col-md-6 mt-md-0 mt-3'>
-              {/* Content */}
-              <h5 className='text-uppercase font-weight-bold'>Footer text 1</h5>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Expedita sapiente sint, nulla, nihil repudiandae commodi
-                voluptatibus corrupti animi sequi aliquid magnam debitis, maxime
-                quam recusandae harum esse fugiat. Itaque, culpa?
-              </p>
-            </div>
-            {/* Grid column */}
-            <hr className='clearfix w-100 d-md-none pb-3' />
-            {/* Grid column */}
-            <div className='col-md-6 mb-md-0 mb-3'>
-              {/* Content */}
-              <h5 className='text-uppercase font-weight-bold'>Footer text 2</h5>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
-                deserunt fuga perferendis modi earum commodi aperiam temporibus
-                quod nulla nesciunt aliquid debitis ullam omnis quos ipsam,
-                aspernatur id excepturi hic.
-              </p>
-            </div>
-            {/* Grid column */}
-          </div>
-          {/* Grid row */}
-        </div>
-        {/* Footer Text */}
-        {/* Copyright */}
-        <div className='footer-copyright text-center py-3'>
-          © 2020 Copyright:
-          <Link to='/'> Reach</Link>
-        </div>
-        {/* Copyright */}
-      </footer>
-      {/* Footer */}
-    </Fragment>
+    <MDBFooter color='mdb-color darken-1' className='font-small pt-4 mt-auto'>
+      <MDBContainer fluid className='text-center text-md-left'>
+        <MDBRow>
+          <MDBCol md='6'>
+            <h5 className='title'>Footer Content</h5>
+            <p>
+              Here you can use rows and columns here to organize your footer
+              content.
+            </p>
+          </MDBCol>
+          <MDBCol md='6'>
+            <h5 className='title'>Links</h5>
+            <ul>
+              <li className='list-unstyled'>
+                <a href='#!'>Link 1</a>
+              </li>
+              <li className='list-unstyled'>
+                <a href='#!'>Link 2</a>
+              </li>
+              <li className='list-unstyled'>
+                <a href='#!'>Link 3</a>
+              </li>
+              <li className='list-unstyled'>
+                <a href='#!'>Link 4</a>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className='footer-copyright text-center py-3'>
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright:{' '}
+          <Link to='/'> Reach </Link>
+        </MDBContainer>
+      </div>
+    </MDBFooter>
   );
 };
 
