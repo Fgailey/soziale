3dc0d77012f8b30a1ccbecee1ab1688b303435a6
Bijan Dayani <bdayani518@gmail.com>
2020-01-30 16:36:05 -0500

    import and add PrivateRoute to path='/dashboard'

--- client/src/routing/Routes.js
+++ client/src/routing/Routes.js
@@ -6,16 +6,16 @@ import Dashboard from '../pages/dashboard/Dashboard';
 import Chat from '../pages/Chat';
 import Profile from '../pages/Profile';
 import NoMatch from '../pages/NoMatch';
-// import PrivateRoute from './PrivateRoute';
+import PrivateRoute from './PrivateRoute';
 
 const Routes = () => (
   <Fragment>
     <Switch>
-      <Route exact path='/dashboard' component={Dashboard} />
-      <Route exact path='/chat' component={Chat} />
-      <Route exact path='/profile' component={Profile} />
-      <Route exact path='/login' component={Login} />
       <Route exact path='/register' component={Register} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/chat' component={Chat} />
+      <PrivateRoute exact path='/dashboard' component={Dashboard} />
+      <Route exact path='/profile' component={Profile} />
       <Route component={NoMatch} />
     </Switch>
   </Fragment>
