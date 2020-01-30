0f077bebde20397c900cf08c54fb46ce39aa6cf6
Bijan Dayani <bdayani518@gmail.com>
2020-01-30 16:03:47 -0500

    /dashboard added import route

--- client/src/routing/Routes.js
+++ client/src/routing/Routes.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
-import Dashboard from '../pages/Dashboard';
+import Dashboard from '../pages/dashboard/Dashboard';
 import Chat from '../pages/Chat';
 import Profile from '../pages/Profile';
 import NoMatch from '../pages/NoMatch';
