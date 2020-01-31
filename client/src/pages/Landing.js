30d92314e529d2436a44ba2c74bedd2f8dfa8389
Bijan Dayani <bdayani518@gmail.com>
2020-01-30 21:50:37 -0500

    redirect to dash if logged in, signup/login btns

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export const Landing = () => {
+export const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
   return (
     <div>
       <div className='landing'>
         <div className='container pt-5 pb-5 text-center'>
-          <h1 className="font-weight-bold">Hello</h1> <br />
-          <h1 className="font-weight-bold">Welcome to Reach</h1> <br />
-          <h1 className="font-weight-bold">I love you</h1> <br />
+          <h1 className='font-weight-bold'>Hello</h1> <br />
+          <h1 className='font-weight-bold'>Welcome to Reach</h1> <br />
+          <h1 className='font-weight-bold'>I love you</h1> <br />
           <iframe
-            title="Intro Vid"
+            title='Intro Vid'
             width={560}
             height={315}
             src='https://youtube.com/embed/ZIFCWpn4qQ4'
@@ -18,9 +24,25 @@ export const Landing = () => {
             allowFullScreen
           />
         </div>
+        <div className='container text-center pb-5'>
+          <Link to='/register' className='btn btn-pink'>
+            <i className='fas fa-clone left' /> Sign Up!
+          </Link>
+          <Link to='/login' className='btn btn-pink'>
+            <i className='fas fa-clone left' /> Log In
+          </Link>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Landing;
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps)(Landing);
