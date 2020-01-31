04bf5cd98b77495bf67c654bb468fe1a9fdb90a8
Scott Ladd <scottjr101@gmail.com>
2020-01-31 01:18:11 -0500

    cleaned up footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,35 +1,35 @@
 import React from 'react';
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from 'mdbreact';
+import { MDBCol, MDBContainer, MDBRow, MDBFooter, MDBBtn } from 'mdbreact';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <MDBFooter color='mdb-color darken-1' className='font-small pt-4 mt-auto'>
-      <MDBContainer fluid className='text-center text-md-left'>
+      <MDBContainer fluid className='text-center text-md-left pb-3'>
         <MDBRow>
-          <MDBCol md='6'>
+          <MDBCol md='6' className='pl-5 pr-5'>
             <h5 className='title'>Footer Content</h5>
             <p>
               Here you can use rows and columns here to organize your footer
               content.
             </p>
           </MDBCol>
-          <MDBCol md='6'>
-            <h5 className='title'>Linkss</h5>
-            <ul>
-              <li className='list-unstyled'>
-                <a href='#!'>Link 1</a>
-              </li>
-              <li className='list-unstyled'>
-                <a href='#!'>Link 2</a>
-              </li>
-              <li className='list-unstyled'>
-                <a href='#!'>Link 3</a>
-              </li>
-              <li className='list-unstyled'>
-                <a href='#!'>Link 4</a>
-              </li>
-            </ul>
+          <MDBCol md='6' className='pl-5 pr-5'>
+            <h5 className='title'>Site Links</h5>
+            <li className='list-unstyled'>
+              <Link to='#!'>
+                <MDBBtn outline color='primary' size='sm'>
+                  Github
+                </MDBBtn>
+              </Link>
+            </li>
+            <li className='list-unstyled'>
+              <Link to='#!'>
+                <MDBBtn outline color='primary' size='sm'>
+                  Other
+                </MDBBtn>
+              </Link>
+            </li>
           </MDBCol>
         </MDBRow>
       </MDBContainer>
