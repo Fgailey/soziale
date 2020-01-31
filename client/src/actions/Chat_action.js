2f2b3c2125a597ca38cbe7c340450441bb0e63d0
Foster Gailey <foster@gaileyhome.com>
2020-01-31 18:05:35 -0500

    utilize chat action

--- client/src/actions/Chat_action.js
+++ client/src/actions/Chat_action.js
@@ -1,23 +1,25 @@
-// import {
-//     GET_CHATS,
-//     AFTER_POST_MESSAGE
-// } from './types';
-// import { CHAT_SERVER } from '../components/Config.js';
+import axios from 'axios';
 
-// export function getChats(){
-//     const request = axios.get(`${CHAT_SERVER}/getChats`)
-//         .then(response => response.data);
+import {
+    GET_CHATS,
+    AFTER_POST_MESSAGE
+} from './Types';
+import { CHAT_SERVER } from '../components/Config.js';
+
+export function getChats(){
+    const request = axios.get(`${CHAT_SERVER}/getChats`)
+        .then(response => response.data);
     
-//     return {
-//         type: GET_CHATS,
-//         payload: request
-//     }
-// }
+    return {
+        type: GET_CHATS,
+        payload: request
+    }
+}
 
-// export function afterPostMessage(data){
+export function afterPostMessage(data){
 
-//     return {
-//         type: AFTER_POST_MESSAGE,
-//         payload: data
-//     }
-// }
+    return {
+        type: AFTER_POST_MESSAGE,
+        payload: data
+    }
+}
