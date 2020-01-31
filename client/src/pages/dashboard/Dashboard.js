e7d119cc3ccfae18609a91d3f2e9f3a1dc348063
Bijan Dayani <bdayani518@gmail.com>
2020-01-30 23:31:08 -0500

    loader gif, check for login, create profile prompt

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -3,19 +3,29 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/Profile';
+import Loader from '../loadingGif/Loader';
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading }
+}) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
 
-  return (
+  return loading && profile === null ? (
+    <Loader />
+  ) : (
     <Fragment>
       <div className='lightWool py-5'>
         <div className='container'>
           <div className='card pl-5 pr-5 py-5'>
+            {/* Logged in user greeting */}
+            <h1>Welcome, {user && user.name}</h1>
+
             <div className='row mx-1'>
-              <div className='col-md-6 mb-4'>
+              <div className='col-lg-6 mb-4'>
                 {/* Card */}
                 <div
                   className='card card-image'
@@ -47,39 +57,19 @@ const Dashboard = ({ getCurrentProfile, auth, profile }) => {
                   </div>
                 </div>
                 {/* Card */}
-              </div>
-              <div className='col-md-6 mb-4'>
-                {/* Card */}
-                <div
-                  className='card card-image'
-                  style={{
-                    backgroundImage:
-                      'url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg)'
-                  }}
-                >
-                  {/* Content */}
-                  <div className='text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4'>
+
+                {profile !== null ? (
+                  <Fragment>has profile</Fragment>
+                ) : (
+                  <Fragment>
                     <div>
-                      <h5 className='pink-text'>
-                        <i className='fas fa-chart-pie' /> Marketing
-                      </h5>
-                      <h3 className='card-title pt-2'>
-                        <strong>This is the card title</strong>
-                      </h3>
-                      <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Repellat fugiat, laboriosam, voluptatem, optio
-                        vero odio nam sit officia accusamus minus error nisi
-                        architecto nulla ipsum dignissimos. Odit sed qui,
-                        dolorum!.
-                      </p>
-                      <Link to='#!' className='btn btn-pink'>
-                        <i className='fas fa-clone left' /> View project
+                      <p>Your profile has not been created.. Yet!</p>
+                      <Link to='/create-profile' className='btn btn-green'>
+                        CREATE PROFILE
                       </Link>
                     </div>
-                  </div>
-                </div>
-                {/* Card */}
+                  </Fragment>
+                )}
               </div>
             </div>
           </div>
