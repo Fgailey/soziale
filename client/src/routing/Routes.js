275da2c51753a095685626c20292f1ba63784b41
Scott Ladd <scottjr101@gmail.com>
2020-01-31 16:39:32 -0500

    private routes

--- client/src/routing/Routes.js
+++ client/src/routing/Routes.js
@@ -6,13 +6,13 @@ import Dashboard from '../pages/Dashboard';
 import Chat from '../pages/Chat';
 import Profile from '../pages/Profile';
 import NoMatch from '../pages/NoMatch';
-// import PrivateRoute from './PrivateRoute';
+import PrivateRoute from './PrivateRoute';
 
 const Routes = () => (
   <Fragment>
     <Switch>
-      <Route exact path='/dashboard' component={Dashboard} />
-      <Route exact path='/chat' component={Chat} />
+      <PrivateRoute exact path='/dashboard' component={Dashboard} />
+      <PrivateRoute exact path='/chat' component={Chat} />
       <Route exact path='/profile' component={Profile} />
       <Route exact path='/login' component={Login} />
       <Route exact path='/register' component={Register} />
