1c6a89f3567713cede5472aadee6da913a7085a9
Ali Rahimlou <Rahimlou91@gmail.com>
2020-02-01 18:41:25 -0500

    cleaning up some code

--- routes/vidyoToken.js
+++ routes/vidyoToken.js
@@ -4,9 +4,6 @@ var vidyoToken = require('../generateToken')
 var cors = require('cors')
 
 
-const config = require('config');
-const { check, validationResult } = require('express-validator');
-
 router.get('/getToken', cors() ,function (req,res){
   var key = vidyoToken(req.body.username,1000, "");
     // console.log(req.body)
