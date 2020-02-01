ceaff13a2e8e677fc75e1002e753288b32c20a44
Bijan Dayani <bdayani518@gmail.com>
2020-02-01 14:25:59 -0500

    mapState2Props, getProfiles props

---
+++ client/src/pages/profiles/Profiles.js
@@ -0,0 +1,20 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Loader from '../loadingGif/Loader';
+import { getProfiles } from '../../actions/Profile';
+
+const Profiles = props => {
+  return <div></div>;
+};
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(mapStateToProps, { getProfiles })(Profiles);
