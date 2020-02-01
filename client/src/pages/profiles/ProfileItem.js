db9b80a8da039b392885597ed4a3a2e9e883f59b
Bijan Dayani <bdayani518@gmail.com>
2020-02-01 17:21:26 -0500

    rough profile summary

--- client/src/pages/profiles/ProfileItem.js
+++ client/src/pages/profiles/ProfileItem.js
@@ -1,1 +1,39 @@
 // Individual profile
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const ProfileItem = ({
+  profile: {
+    user: { _id, name, avatar },
+    age,
+    current_city,
+    interests,
+    date
+  }
+}) => {
+  return (
+    <div className='profile-item'>
+      <img src={avatar} alt='' />
+      <div>
+        <h2>{name}</h2>
+        <p>Age: {age}</p>
+        <p>Location: {current_city}</p>
+        <Link to={`/profile/${_id}`} className='btn btn-primary'>
+          View Profile
+        </Link>
+      </div>
+      <ul>
+        {interests.slice(0, 4).map((interest, index) => (
+          <li key={index} className='text=primary'>
+            {interest}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+ProfileItem.propTypes = {};
+
+export default ProfileItem;
