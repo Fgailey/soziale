786970a8e6fd072370ddef3c99c3e5974262dbf7
Foster Gailey <foster@gaileyhome.com>
2020-02-01 11:12:09 -0500

    added reux-promise to middleware

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise';
 import rootReducer from './reducers';
 
 const initialState = {};
 
-const middleware = [thunk];
+const middleware = [promiseMiddleware, thunk];
 
 const store = createStore(
   rootReducer,
