e0123d4b70e4a1db56e511d7dbe4f41fe6f0835c
Ali Rahimlou <Rahimlou91@gmail.com>
2020-02-01 18:06:08 -0500

    changes expire time

--- routes/vidyoToken.js
+++ routes/vidyoToken.js
@@ -8,7 +8,7 @@ const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 router.get('/getToken', cors() ,function (req,res){
-  var key = vidyoToken(req.body.username,10000, "");
+  var key = vidyoToken(req.body.username,1000, "");
     // console.log(req.body)
 res.json({token: key })
 })
