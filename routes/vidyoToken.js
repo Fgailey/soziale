188b54c8be25119830648cb6612d2d95d25eec60
Ali Rahimlou <Rahimlou91@gmail.com>
2020-02-01 15:10:51 -0500

    merged

--- routes/vidyoToken.js
+++ routes/vidyoToken.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
 var vidyoToken = require('../generateToken')
+var cors = require('cors')
+// var axios = require("axios");
 
 const config = require('config');
 const { check, validationResult } = require('express-validator');
 
-router.get('/getToken',function (req,res){
+router.get('/getToken', cors() ,function (req,res){
   var key = vidyoToken(req.body.username,10000, "");
     // console.log(req.body)
 res.json({token: key })
 })
+
 
 
 
