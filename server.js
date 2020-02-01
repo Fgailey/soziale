d73f0668a74ec218a46e35500e5cef063263d669
Foster Gailey <foster@gaileyhome.com>
2020-01-31 19:21:26 -0500

    mongo in server merge

--- server.js
+++ server.js
@@ -7,8 +7,22 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+<<<<<<< HEAD
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+=======
+const server = require('http').createServer(app)
+const io = require("socket.io")(server)
+
+const mongoose = require("mongoose");
+const config = require('./config/default.json')
+
+const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+
+>>>>>>> added mongoose connection
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: false }));
@@ -24,13 +38,20 @@ if (process.env.NODE_ENV === 'production') {
 // Define API routes here
 app.use('/users', require('./routes/users'));
 app.use('/auth', require('./routes/auth'));
+<<<<<<< HEAD
 app.use('/profile', require('./routes/profile'));
 
 // const { Chat } = require("./models/Chat");
+=======
+
+//Define Models here
+const { Chat } = require("./models/Chat");
+>>>>>>> added mongoose connection
 
 io.on('connection', socket => {
   console.log('made socket connection', socket.id);
 
+<<<<<<< HEAD
   socket.on('chat', data => {
     io.sockets.emit('chat', data);
     console.log('chat data: ' + data.chatMessage);
@@ -48,6 +69,29 @@ io.on('connection', socket => {
   //         Chat.find({ "_id": doc._id })
   //         .populate("sender")
   //         .exec((err, doc)=> {
+=======
+  socket.on("Input Chat Message", msg => {
+    
+    connect.then(db => {
+      try {
+          let chat = new Chat({ message: msg.chatMessage, sender:msg.userID, type: msg.type })
+
+          chat.save((err, doc) => {
+            if(err) return res.json({ success: false, err })
+
+            Chat.find({ "_id": doc._id })
+            .populate("sender")
+            .exec((err, doc)=> {
+
+                return io.emit("Output Chat Message", doc);
+            })
+          })
+      } catch (error) {
+        console.error(error);
+      }
+    })
+   })
+>>>>>>> added mongoose connection
 
   //             return io.emit("Output Chat Message", doc);
   //         })
