cb649813b27670507380ac34b88847dbc98bd265
Ali Rahimlou <Rahimlou91@gmail.com>
2020-02-01 16:19:16 -0500

    edit

--- routes/vidyoToken.js
+++ routes/vidyoToken.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 var vidyoToken = require('../generateToken')
 var cors = require('cors')
-// var axios = require("axios");
+
 
 const config = require('config');
 const { check, validationResult } = require('express-validator');
