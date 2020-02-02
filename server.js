f0243855c86a3e91f13cef93568eedab4f338bdb
Foster Gailey <foster@gaileyhome.com>
2020-02-01 22:39:39 -0500

    moved static file

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const express = require("express");
-const path = require("path");
-var axios = require("axios");
+const express = require('express');
+const path = require('path');
+var axios = require('axios');
 const connectDB = require('./config/db');
-var cors = require('cors')
-
+var cors = require('cors');
 
 const app = express();
 
@@ -17,9 +16,10 @@ const io = require('socket.io')(server);
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: false }));
+// Serve up static assets (usually on heroku)
 
 // Define API routes here
-app.use(cors())
+app.use(cors());
 app.use('/users', require('./routes/users'));
 app.use('/auth', require('./routes/auth'));
 app.use('/profile', require('./routes/profile'));
@@ -27,45 +27,44 @@ app.use('/posts', require('./routes/posts'));
 app.use('/chat', require('./routes/chat'));
 app.use('/vidyoToken', require('./routes/vidyoToken'));
 
-
-
-
-
-
-
-
 // Serve up static assets (usually on heroku)
 
 //Define Models here
-const { Chat } = require("./models/Chat");
-
+const { Chat } = require('./models/Chat');
 
 io.on('connection', socket => {
   console.log('made socket connection', socket.id);
 
-  socket.on("Input Chat Message", msg => {
-    
+  socket.on('Input Chat Message', msg => {
     //because there is an open serve the db does not need to be called again
-      try {
-          let chat = new Chat({ message: msg.chatMessage, sender:msg.userID, type: msg.type })
+    try {
+      let chat = new Chat({
+        message: msg.chatMessage,
+        sender: msg.userID,
+        type: msg.type
+      });
 
-          chat.save((err, doc) => {
-            if(err) return res.json({ success: false, err })
+      chat.save((err, doc) => {
+        if (err) return res.json({ success: false, err });
 
-            Chat.find({ "_id": doc._id })
-            .populate("sender")
-            .exec((err, doc)=> {
+        Chat.find({ _id: doc._id })
+          .populate('sender')
+          .exec((err, doc) => {
+            return io.emit('Output Chat Message', doc);
+          });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  });
+});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
-                return io.emit("Output Chat Message", doc);
-            })
-          })
-      } catch (error) {
-        console.error(error);
-      }
-   })
-
-})
-
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
