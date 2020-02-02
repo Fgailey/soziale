8916e72891c27a3eeaecf3873881977e6dc77e14
Bijan Dayani <bdayani518@gmail.com>
2020-02-01 22:56:54 -0500

    match.params.id dep added 2 effect hook(warning)

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -13,7 +13,7 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfilesById(match.params.id);
-  }, [getProfilesById]);
+  }, [getProfilesById, match.params.id]);
 
   return (
     <Fragment>
