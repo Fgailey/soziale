73f77687ccb6d7796efee118381f2dfe7640d014
Scott Ladd <scottjr101@gmail.com>
2020-02-02 14:54:11 -0500

    testing

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -8,7 +8,7 @@ const NoMatch = () => {
         <h1>404 Page Not Found</h1>
         <h1>
             <span role="img" aria-label="Face With Rolling Eyes Emoji">
-            🙄welp
+            🙄
             </span>
         </h1>
    </div>
