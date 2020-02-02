1593b84ba0a9bd60b902b75b10c98d93aed2f4ec
Scott Ladd <scottjr101@gmail.com>
2020-02-01 23:52:16 -0500

    moved routes

--- server.js
+++ server.js
@@ -2,26 +2,25 @@ const express = require('express');
 const path = require('path');
 const connectDB = require('./config/db');
 
+const app = express();
+
 // Connect Database
 connectDB();
 
 const PORT = process.env.PORT || 5000;
-const app = express();
-const server = require('http').createServer(app)
-const io = require("socket.io")(server)
 
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: false }));
 
 // Define API routes here
 app.use('/users', require('./routes/users'));
 app.use('/auth', require('./routes/auth'));
 app.use('/profile', require('./routes/profile'));
 app.use('/posts', require('./routes/posts'));
-app.use('/chat', require('./routes/chat'));
 
-
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ extended: false }));
 // Serve up static assets (usually on heroku)
 
 
