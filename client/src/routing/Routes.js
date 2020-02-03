4b82a103ee950d716b03041b0226e759910fae4f
Scott Ladd <scottjr101@gmail.com>
2020-02-03 00:03:43 -0500

    added profile to private route

--- client/src/routing/Routes.js
+++ client/src/routing/Routes.js
@@ -18,14 +18,14 @@ const Routes = () => (
     <Switch>
       <Route exact path='/register' component={Register} />
       <Route exact path='/login' component={Login} />
-      <Route exact path='/profiles' component={Profiles} />
+      <PrivateRoute exact path='/profiles' component={Profiles} />
       <PrivateRoute exact path='/dashboard' component={Dashboard} />
       <PrivateRoute exact path='/create-profile' component={CreateProfile} />
       <PrivateRoute exact path='/edit-profile' component={EditProfile} />
       <PrivateRoute exact path='/posts' component={Posts} />
       <PrivateRoute exact path='/posts/:id' component={Post} />
       <PrivateRoute exact path='/chat' component={Chat} />
-      <Route exact path='/profile/:id' component={Profile} />
+      <PrivateRoute exact path='/profile/:id' component={Profile} />
       <PrivateRoute exact path='/profile' component={Profile} />
       <Route component={NoMatch} />
     </Switch>
