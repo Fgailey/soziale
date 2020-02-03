ace7e7b732addef6fb7052b37e97fe6b7d80086d
Bijan Dayani <bdayani518@gmail.com>
2020-02-03 14:26:51 -0500

    mdbootstrap classes added

--- client/src/pages/profiles/ProfileItem.js
+++ client/src/pages/profiles/ProfileItem.js
@@ -13,27 +13,37 @@ const ProfileItem = ({
   }
 }) => {
   return (
-    <div className='profile-item'>
-      <img src={avatar} alt='' />
-      <div>
-        <h2>{name}</h2>
-        <p>Age: {age}</p>
-        <p>Location: {current_city}</p>
+    <div className='container'>
+      <div className='card mx-5 my-5'>
+        <div className='profile-item col-4'>
+          <img src={avatar} alt='' />
+          <div>
+            <h2>{name}</h2>
+            <p>Age: {age}</p>
+            <p>Location: {current_city}</p>
+          </div>
+        </div>
+
+        <div className='col-4'>
+          <ul>
+            {interests.slice(0, 4).map((interest, index) => (
+              <li key={index} className='text=primary'>
+                {interest}
+              </li>
+            ))}
+          </ul>
+        </div>
+
         <Link to={`/profile/${_id}`} className='btn btn-primary'>
           View Profile
         </Link>
       </div>
-      <ul>
-        {interests.slice(0, 4).map((interest, index) => (
-          <li key={index} className='text=primary'>
-            {interest}
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
 
-ProfileItem.propTypes = {};
+ProfileItem.propTypes = {
+  profile: PropTypes.object.isRequired
+};
 
 export default ProfileItem;
