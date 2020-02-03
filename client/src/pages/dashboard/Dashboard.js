c8ab830efca4e1c22a8e5b5707a9db6a7862600b
Ali Rahimlou <Rahimlou91@gmail.com>
2020-02-03 14:38:02 -0500

    vidyo modal

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -4,20 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/Profile';
 import Loader from '../loadingGif/Loader';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Alert from '../Alert';
-=======
-import ModalPage from '../../components/modal/vidyoModal'
-import VidyoConnector  from '../vidyo'
->>>>>>> vidyo modal
-=======
-
->>>>>>> workin workin
-=======
-import ModalPage from '../../components/modal/vidyoModal'
->>>>>>> struggles
 
 const Dashboard = ({
   getCurrentProfile,
@@ -31,7 +18,6 @@ const Dashboard = ({
   return loading && profile === null ? (
     <Loader />
   ) : (
-    
     <Fragment>
       <div className='container my-5'>
         <div className='card p-5'>
@@ -39,8 +25,6 @@ const Dashboard = ({
           <h1>Welcome, {user && user.name}</h1>
           <Alert />
 
-<<<<<<< HEAD
-<<<<<<< HEAD
           <div className='row mx-1'>
             <div className='col-lg-6 mb-4'>
               {/* Card */}
@@ -69,65 +53,8 @@ const Dashboard = ({
                     <Link to='#!' className='btn btn-pink'>
                       <i className='fas fa-clone left' /> View project
                     </Link>
-=======
-                
-=======
-                <ModalPage/>
->>>>>>> struggles
-
-            <div className='row mx-1'>
-              <div className='col-lg-6 mb-4'>
-                {/* Card */}
-                <div
-                  className='card card-image'
-                  style={{
-                    backgroundImage:
-                      'url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg)'
-                  }}
-                >
-                  {/* Content */}
-                  <div className='text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4'>
-                    <div>
-                      <h5 className='pink-text'>
-                        <i className='fas fa-chart-pie' /> Marketing
-                      </h5>
-                      <h3 className='card-title pt-2'>
-                        <strong>This is the card title</strong>
-                      </h3>
-                      <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Repellat fugiat, laboriosam, voluptatem, optio
-                        vero odio nam sit officia accusamus minus error nisi
-                        architecto nulla ipsum dignissimos. Odit sed qui,
-                        dolorum!.
-                      </p>
-                      <Link to='#!' className='btn btn-pink'>
-                        <i className='fas fa-clone left' /> View project
-                      </Link>
-                    </div>
->>>>>>> workin workin
                   </div>
                 </div>
-<<<<<<< HEAD
-=======
-                {/* Card */}
-                      
-                
-
-                {profile !== null ? (
-                  <Fragment>has profile</Fragment>
-                ) : (
-                  <Fragment>
-                    <div>
-                      <p>Your profile has not been created.. Yet!</p>
-                      <Link to='/create-profile' className='btn btn-green'>
-                        CREATE PROFILE
-                          
-                      </Link>
-                    </div>
-                  </Fragment>
-                )}
->>>>>>> vidyo modal
               </div>
               {/* Card */}
 
@@ -147,9 +74,7 @@ const Dashboard = ({
           </div>
         </div>
       </div>
-      
     </Fragment>
-    
   );
 };
 
