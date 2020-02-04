0e0d1305cf70a92ce9b0888538722e9727f2fbd1
Scott Ladd <scottjr101@gmail.com>
2020-02-04 02:35:25 -0500

    added route for video chat

--- client/src/routing/Routes.js
+++ client/src/routing/Routes.js
@@ -6,11 +6,13 @@ import Dashboard from '../pages/dashboard/Dashboard';
 import CreateProfile from '../components/profile-forms/CreateProfile';
 import EditProfile from '../components/profile-forms/EditProfile';
 import Chat from '../pages/Chat';
+import VideoChat from '../pages/vidyo';
 import Profile from '../pages/profile/Profile';
 import Profiles from '../pages/profiles/Profiles';
 import NoMatch from '../pages/NoMatch';
 import Posts from '../components/posts/Posts';
 import Post from '../components/post/Post';
+import FileUpload from '../components/file-upload';
 import PrivateRoute from './PrivateRoute';
 
 const Routes = () => (
@@ -25,8 +27,10 @@ const Routes = () => (
       <PrivateRoute exact path='/posts' component={Posts} />
       <PrivateRoute exact path='/posts/:id' component={Post} />
       <PrivateRoute exact path='/chat' component={Chat} />
+      <PrivateRoute exact path='/video-chat' component={VideoChat} />
       <PrivateRoute exact path='/profile/:id' component={Profile} />
       <PrivateRoute exact path='/profile' component={Profile} />
+      <PrivateRoute exact path='/picture' component={FileUpload} />
       <Route component={NoMatch} />
     </Switch>
   </Fragment>
