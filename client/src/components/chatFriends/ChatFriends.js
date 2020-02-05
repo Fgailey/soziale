f1070c4ddcbef123d9b30c0896ee06500c4fbdbf
Foster Gailey <foster@gaileyhome.com>
2020-02-04 19:22:02 -0500

    added profiles to chat page

---
+++ client/src/components/chatFriends/ChatFriends.js
@@ -0,0 +1,38 @@
+import React, {useEffect} from "react";
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getProfiles } from '../../actions/Profile';
+import ChatFriendsItem from "./ChatFriendsItem";
+
+const ChatFriends = ({ getProfiles, profile: { profiles } }) => {
+  useEffect(() => {
+    getProfiles();
+  }, [getProfiles]);
+
+  return (
+    <div>
+
+          <p>Select a user to message them Directly</p>
+          <div className='profiles-div'>
+            {profiles.length > 0 ? (
+              profiles.map(profile => (
+                <ChatFriendsItem key={profile._id} profile={profile} />
+              ))
+            ) : (
+              <h4>No profiles found...</h4>
+            )}
+          </div>
+    </div>
+  );
+}
+
+ChatFriends.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(mapStateToProps, { getProfiles })(ChatFriends);
