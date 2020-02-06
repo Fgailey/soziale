8037bc241af6fbf473c9acd6f632aada29abd756
Foster Gailey <foster@gaileyhome.com>
2020-02-06 10:06:57 -0500

    chat bar member layout change

--- client/src/components/chatFriends/ChatFriendsItem.js
+++ client/src/components/chatFriends/ChatFriendsItem.js
@@ -7,11 +7,21 @@ const ChatFriendsItem = ({
   }
 }) => {
   return (
-    <div className='profile-item' id={_id}>
-      <div>
-        <h2>{name}</h2>
-      </div>
-    </div>
+
+    <li className="active lighten-3 p-2" id={_id}>
+      <a href="#" className="d-flex justify-content-between">
+        <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-8.jpg" alt="avatar" className="avatar rounded-circle d-flex align-self-center mr-2 z-depth-1" />
+        <div className="text-small">
+          <strong>{name}</strong>
+          <p className="last-message text-muted">Hello, Are you there?</p>
+        </div>
+        <div className="chat-footer">
+          <p className="text-smaller text-muted mb-0">Just now</p>
+          {/* <span className="badge badge-danger float-right">1</span> */}
+        </div>
+      </a>
+    </li>
+    
   );
 };
 
