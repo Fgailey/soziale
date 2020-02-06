90304050988734c4409be99c088656e3c999e7f2
Bijan Dayani <bdayani518@gmail.com>
2020-02-06 14:13:07 -0500

    profile item css/layout

--- client/src/pages/profiles/ProfileItem.js
+++ client/src/pages/profiles/ProfileItem.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import './profileItem.css';
 
 const ProfileItem = ({
   profile: {
@@ -14,22 +15,23 @@ const ProfileItem = ({
 }) => {
   return (
     <div className='container'>
-      <div className='card mx-5 my-5'>
+      <div className='card mx-5 my-5 morpheus-den-gradient'>
         <div className='row'>
-          <div className='profile-item col-4 ml-4'>
+          <div className='profile-info col text-center mx-4 my-4'>
             <img
-              className='img-thumbnail w-25 float-left'
+              className='img-thumbnail dusty-grass-gradient w-25 ml-5 rounded-circle'
               src={avatar}
               alt=''
             />
-            <div>
+            <div className='ml-4 mt-3'>
               <h2>{name}</h2>
               <p>Age: {age}</p>
               <p>Location: {current_city}</p>
             </div>
           </div>
 
-          <div className='col-4'>
+          <div className='interests col mt-5'>
+            <h5>Some of {name}'s interests include: </h5>
             <ul>
               {interests.slice(0, 4).map((interest, index) => (
                 <li key={index} className='text=primary'>
@@ -40,7 +42,7 @@ const ProfileItem = ({
           </div>
         </div>
 
-        <Link to={`/profile/${_id}`} className='btn btn-primary col-6'>
+        <Link to={`/profile/${_id}`} className='btn blue-gradient btn-rounded'>
           View Profile
         </Link>
       </div>
