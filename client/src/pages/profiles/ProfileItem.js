c5f22d5ee3d1b8ba6dcf9ec6b82be81c9f8554a0
Bijan Dayani <bdayani518@gmail.com>
2020-02-06 18:15:29 -0500

    font colors, bg color, gradients

--- client/src/pages/profiles/ProfileItem.js
+++ client/src/pages/profiles/ProfileItem.js
@@ -15,26 +15,26 @@ const ProfileItem = ({
 }) => {
   return (
     <div className='container'>
-      <div className='card mx-5 my-5 morpheus-den-gradient'>
+      <div className='card mx-5 my-5' id='profileItemCard'>
         <div className='row'>
           <div className='profile-info col text-center mx-4 my-4'>
             <img
-              className='img-thumbnail dusty-grass-gradient w-25 ml-5 rounded-circle'
+              className='img-thumbnail aqua-gradient w-25 ml-5 rounded-circle'
               src={avatar}
               alt=''
             />
             <div className='ml-4 mt-3'>
-              <h2>{name}</h2>
-              <p>Age: {age}</p>
-              <p>Location: {current_city}</p>
+              <h2 className='prim'>{name}</h2>
+              <p className='sec'>Age: {age}</p>
+              <p className='sec'>Location: {current_city}</p>
             </div>
           </div>
 
           <div className='interests col mt-5'>
-            <h5>Some of {name}'s interests include: </h5>
+            <h5 className='prim'>{name}'s interests: </h5>
             <ul>
               {interests.slice(0, 4).map((interest, index) => (
-                <li key={index} className='text=primary'>
+                <li key={index} className='text=primary sec'>
                   {interest}
                 </li>
               ))}
@@ -42,7 +42,7 @@ const ProfileItem = ({
           </div>
         </div>
 
-        <Link to={`/profile/${_id}`} className='btn blue-gradient btn-rounded'>
+        <Link to={`/profile/${_id}`} className='btn aqua-gradient btn-rounded'>
           View Profile
         </Link>
       </div>
