51d9d6f7aa0bba7b5e2d11a8e1b52d1d45419072
Bijan Dayani <bdayani518@gmail.com>
2020-02-08 17:35:56 -0500

    add about_me, put posts cmpnt in card

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -20,67 +20,70 @@ const Dashboard = ({
     <Loader />
   ) : (
     <Fragment>
-      <div className='container my-5'>
-        <div className='card p-5'>
-          {/* Logged in user greeting */}
-          <h1>Welcome, {user && user.name}</h1>
-          <Alert />
+      <div className='container'>
+        {/* Logged in user greeting */}
+        <h1>Welcome, {user && user.name}</h1>
+        <Alert />
 
-          {profile !== null ? (
-            <Fragment>
-              {/* Card */}
-              <div className='row'>
-                <div className='card col-5 blue'>
+        {profile !== null ? (
+          <Fragment>
+            {/* Card */}
+            <div className='row'>
+              <div className='card col-5 blue'>
+                <div className='row'>
+                  <div className='card'></div>
                   <div className='row'>
-                    <div className='card'></div>
-                    <div className='row'>
-                      <div className='col-6 '>
-                        <img
-                          src={user.avatar}
-                          alt=''
-                          className='img-thumbnail ml-2 my-2 aqua-gradient rounded-circle'
-                        />
+                    <div className='col-6 '>
+                      <img
+                        src={user.avatar}
+                        alt=''
+                        className='img-thumbnail ml-2 my-2 aqua-gradient rounded-circle'
+                      />
+                    </div>
+                    <div className='col-6 pt-5 pl-5 align-bottom'>
+                      <div className='row details'>
+                        <h2 className='prim'>Age: </h2>
+                        <h3 className='mt-1'>{profile.age}</h3>
                       </div>
-                      <div className='col-6 pt-5 pl-5 align-bottom'>
-                        <div className='row details'>
-                          <h2 className='prim'>Age: </h2>
-                          <h3 className='mt-1'>{profile.age}</h3>
-                        </div>
-                        <div className='row details'>
-                          <h2 className='prim'>Location: </h2>
-                          <h3 className='mt-1'>{profile.current_city}</h3>
-                        </div>
-                        <div className='row details'>
-                          <h2 className='prim'>Gender: </h2>
-                          <h3 className='mt-1'>{profile.gender}</h3>
-                        </div>
+                      <div className='row details'>
+                        <h2 className='prim'>Location: </h2>
+                        <h3 className='mt-1'>{profile.current_city}</h3>
+                      </div>
+                      <div className='row details'>
+                        <h2 className='prim'>Gender: </h2>
+                        <h3 className='mt-1'>{profile.gender}</h3>
                       </div>
                     </div>
                   </div>
-                  <div className='row'>
-                    <div className='col-12'>about</div>
-                  </div>
-                  <div className='row p-2 green'>
-                    <div className='card col-12'>following/followers</div>
+                </div>
+                <div className='row'>
+                  <div className='col-12'>
+                    <h2 className='prim'>About: </h2>
+                    <h3 className='mt-1'>{profile.about_me}</h3>
                   </div>
                 </div>
-                <div className=' card col-7 p-2'>
+                <div className='row p-2 green'>
+                  <div className='card col-12'>following/followers</div>
+                </div>
+              </div>
+              <div className='col-7'>
+                <div className='card col-12'>
                   <PostsDashboard />
                 </div>
               </div>
-              {/* Card */}
-            </Fragment>
-          ) : (
-            <Fragment>
-              <div>
-                <p>Your profile has not been created.. Yet!</p>
-                <Link to='/create-profile' className='btn btn-green'>
-                  CREATE PROFILE
-                </Link>
-              </div>
-            </Fragment>
-          )}
-        </div>
+            </div>
+            {/* Card */}
+          </Fragment>
+        ) : (
+          <Fragment>
+            <div>
+              <p>Your profile has not been created.. Yet!</p>
+              <Link to='/create-profile' className='btn btn-green'>
+                CREATE PROFILE
+              </Link>
+            </div>
+          </Fragment>
+        )}
       </div>
       <Footer />
     </Fragment>
