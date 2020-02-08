e48f9c0b90ee7eb54fbcdaaaa9be4f165a801c23
Bijan Dayani <bdayani518@gmail.com>
2020-02-08 16:45:30 -0500

    same display setup as dashboard (temporary)

--- client/src/pages/profile/ProfileUser.js
+++ client/src/pages/profile/ProfileUser.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import PostsDashboard from '../../components/posts/PostsDashboard';
 // import './profileUser.css';
 
 const ProfileUser = ({
@@ -14,16 +15,47 @@ const ProfileUser = ({
   return (
     // maybe move this layout to dashboard route?
     <div className='row'>
-      <div className='col-5 blue'>
-        <div className='row p-5 red'>
-          <div className='card col-12'>profile</div>
+      <div className='card col-5 blue'>
+        <div className='row'>
+          <div className='card'></div>
+          <div className='row'>
+            <div className='col-6 '>
+              <img
+                src={avatar}
+                alt=''
+                className='img-thumbnail ml-2 my-2 aqua-gradient rounded-circle'
+              />
+            </div>
+            <div className='col-6 pt-5 pl-5 align-bottom'>
+              <div className='row details'>
+                <h2 className='prim'>Age: </h2>
+                <h3 className='mt-1'>{age}</h3>
+              </div>
+              <div className='row details'>
+                <h2 className='prim'>Location: </h2>
+                <h3 className='mt-1'>{current_city}</h3>
+              </div>
+              <div className='row details'>
+                <h2 className='prim'>Gender: </h2>
+                <h3 className='mt-1'>{gender}</h3>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className='row p-5 green'>
+        <div className='row'>
+          <div className='col-12'>
+            <h2 className='prim'>About: </h2>
+            <h3 className='mt-1'>{about_me}</h3>
+          </div>
+        </div>
+        <div className='row p-2 green'>
           <div className='card col-12'>following/followers</div>
         </div>
       </div>
-      <div className='col-7 p-5 purple'>
-        <div className='card col-12'>user's posts</div>
+      <div className='col-7'>
+        <div className='card col-12 h-100'>
+          <PostsDashboard />
+        </div>
       </div>
     </div>
   );
