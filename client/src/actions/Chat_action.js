4913839d025781eda081f80bfc8a26a5b2170e53
Foster Gailey <foster@gaileyhome.com>
2020-02-08 14:36:35 -0500

    added default and new room actions

--- client/src/actions/Chat_action.js
+++ client/src/actions/Chat_action.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 import {
     GET_CHATS,
-    // GET_PRIVATE_CHATS,
-    AFTER_POST_MESSAGE
+    AFTER_POST_MESSAGE,
+    GET_NEW_ROOM,
+    GET_DEFAULT_ROOM
 } from './Types';
 import { CHAT_SERVER } from '../components/Config.js';
 
@@ -17,16 +18,24 @@ export function getChats(){
     }
 }
 
-// export function getPrivateChats(user1, user2){
-//     const request = axios.get(`${CHAT_SERVER}/getChats/${user1}+${user2}`)
-//         .then(response => response.data);
-    
-//     return {
-//         type: GET_PRIVATE_CHATS,
-//         payload: request
-//     }
-// }
 
+export function setRoom(user1, user2){
+    const roomID = user1+user2
+    const newRoom = roomID.split('').sort().join('')
+
+    
+    return {
+        type: GET_NEW_ROOM,
+        payload: newRoom
+    }
+}
+export function setRoomDefault(){
+
+    return {
+        type: GET_DEFAULT_ROOM,
+        payload: "community"
+    }
+}
 
 
 export function afterPostMessage(data){
