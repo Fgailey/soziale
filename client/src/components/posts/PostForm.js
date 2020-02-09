e215e3248caa6f03bd69e8733f8736e2e5f7d10c
Scott Ladd <scottjr101@gmail.com>
2020-02-08 23:37:58 -0500

    added class name

--- client/src/components/posts/PostForm.js
+++ client/src/components/posts/PostForm.js
@@ -17,7 +17,7 @@ const PostForm = ({ addPost }) => {
         }}
       >
         <textarea
-          class='form-control z-depth-1'
+          className='form-control z-depth-1'
           name='text'
           rows='5'
           placeholder='Write something here...'
