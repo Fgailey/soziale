5d45393f4ea19c4553597b0fe9e7a8211e1cfcca
Foster Gailey <foster@gaileyhome.com>
2020-02-08 23:27:17 -0500

    Added avatars and time stamps

--- client/src/components/layout/Layout.js
+++ client/src/components/layout/Layout.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { connect } from 'react-redux';
+import Moment from 'react-moment';
 function Layout (props) {
         return(
             <div>
             { (props.room === props.type) ?
                 (props.user._id !== props.sender._id ?
               <li className="d-flex mb-4 pb-3">
-                    <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-6.jpg" alt="avatar" className="avatar rounded-circle mr-2 ml-lg-3 ml-0 z-depth-1" />
+                    {(!props.sender.avatar)?
+                    <img src={"/uploads/default.png"} alt="avatar" className="avatar rounded-circle mr-2 ml-lg-3 ml-0 z-depth-1" />
+                    :
+                    <img src={"/uploads/"+props.sender.avatar} alt="avatar" className="avatar rounded-circle mr-2 ml-lg-3 ml-0 z-depth-1" />
+                    }
                     <div className="chat-body pull-left white p-3 ml-2 z-depth-1">
                         <div className="header">
                         <strong className="primary-font">{props.sender.name}</strong>
-                        <small className="pull-right text-muted"><i className="far fa-clock" /> 12 mins ago</small>
+                        <small className="pull-right text-muted"><i className="far fa-clock" /><Moment fromNow>{props.createdAt}</Moment></small>
                         </div>
                         <hr className="w-100" />
                         <p className="mb-0">
@@ -23,18 +28,22 @@ function Layout (props) {
                     <div className="chat-body pull-left white p-3 ml-2 z-depth-1">
                         <div className="header">
                         <strong className="primary-font">{props.sender.name}</strong>
-                        <small className="pull-right text-muted"><i className="far fa-clock" /> 12 mins ago</small>
+                        <small className="pull-right text-muted"><i className="far fa-clock" /><Moment fromNow>{props.createdAt}</Moment></small>
                         </div>
                         <hr className="w-100" />
                         <p className="mb-0">
                         {props.message}
                         </p>
                     </div>
-                    <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-6.jpg" alt="avatar" className="avatar rounded-circle mr-2 ml-lg-3 ml-0 z-depth-1" />
+                    {(!props.sender.avatar)?
+                    <img src={"/uploads/default.png"} alt="avatar" className="avatar rounded-circle mr-2 ml-lg-3 ml-0 z-depth-1" />
+                    :
+                    <img src={"/uploads/"+props.sender.avatar} alt="avatar" className="avatar rounded-circle mr-2 ml-lg-3 ml-0 z-depth-1" />
+                    }
                 </li>)
-                :
-                null
-                }
+            :
+            null
+            }
 
             </div>
         )
