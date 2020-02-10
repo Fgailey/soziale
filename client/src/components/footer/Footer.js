8dda37efe196a2468f65f04b613a1c1dd36c1cef
Scott Ladd <scottjr101@gmail.com>
2020-02-10 00:19:59 -0500

    topped off

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -8,28 +8,38 @@ const Footer = () => {
       <MDBContainer fluid className='text-center text-md-left pb-3'>
         <MDBRow>
           <MDBCol md='6' className='pl-5 pr-5'>
-            <h5 className='title'>Footer Content</h5>
-            <p>
-              Here you can use rows and columns here to organize your footer
-              content.
+            <h5 className='title mb-1 text-left'>More Info.</h5>
+            <div
+              className='border-bottom border-info mb-2'
+              style={{ width: 60 }}
+            />
+            <p className='mb-0 text-left'>
+              Click on the buttons over yonder to see our source code on Github
+              and to reach out to us with any questions, comments, or concerns.
             </p>
           </MDBCol>
           <MDBCol md='6' className='pl-5 pr-5'>
-            <h5 className='title'>Site Links</h5>
-            <li className='list-unstyled'>
-              <a href='https://github.com/Fgailey/Reach'>
-                <MDBBtn outline color='primary' size='sm'>
-                  Github
-                </MDBBtn>
-              </a>
-            </li>
-            <li className='list-unstyled'>
-              <Link to='/picture'>
-                <MDBBtn outline color='primary' size='sm'>
-                  Picture Upload
-                </MDBBtn>
-              </Link>
-            </li>
+            <h5 className='title mb-1 text-left'>Site Links</h5>
+            <div
+              className='border-bottom border-info mb-2'
+              style={{ width: 60 }}
+            />
+            <MDBContainer fluid className='d-flex justify-content-start pl-0'>
+              <li className='list-unstyled'>
+                <a href='https://github.com/Fgailey/Reach'>
+                  <MDBBtn outline color='warning' size='sm' className='ml-0'>
+                    Github
+                  </MDBBtn>
+                </a>
+              </li>
+              <li className='list-unstyled'>
+                <Link to='/picture'>
+                  <MDBBtn outline color='warning' size='sm' className='ml-0'>
+                    Picture Upload
+                  </MDBBtn>
+                </Link>
+              </li>
+            </MDBContainer>
           </MDBCol>
         </MDBRow>
       </MDBContainer>
