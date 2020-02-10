1bb9ddd16b938740cb531d54ca5ed7bb695a9013
Bijan Dayani <bdayani518@gmail.com>
2020-02-10 00:21:35 -0500

    add Container, Header, min style, profile name

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -5,7 +5,8 @@ import Loader from '../loadingGif/Loader';
 import { getProfilesById } from '../../actions/Profile';
 import { Link } from 'react-router-dom';
 import ProfileUser from './ProfileUser';
-import Footer from '../../components/footer/Footer'
+import Alert from '../Alert';
+import Footer from '../../components/footer/Footer';
 
 const Profile = ({
   getProfilesById,
@@ -23,22 +24,33 @@ const Profile = ({
         <Loader />
       ) : (
         <Fragment>
-          <div className='container'>
-            <Link to='/profiles' className='btn'>
-              Go back
+          <div className='row' id='profilesHeader'>
+            <Link to='/profiles' className='btn aqua-gradient float-left'>
+              <i className='black-text fas fa-arrow-left fa-3x'></i>
             </Link>
+
+            <h1 className='prim mt-3 center'>{profile.user.name}</h1>
+
             {auth.isAuthenticated &&
               auth.loading === false &&
               auth.user._id === profile.user._id && (
-                <Link to='/edit-profile' className='btn'>
-                  Edit Your Profile
+                <Link
+                  to='/edit-profile'
+                  className='btn dusty-grass-gradient float-right'
+                >
+                  <h5 className='black-text'>Edit Your Profile</h5>
                 </Link>
               )}
+          </div>
+          <div className='aqua-gradient' id='borderBottom'></div>
+
+          <Alert />
+          <div className='container py-5 px-5' id='profileContainer'>
             <ProfileUser profile={profile} />
           </div>
         </Fragment>
       )}
-      <Footer/>
+      <Footer />
     </Fragment>
   );
 };
