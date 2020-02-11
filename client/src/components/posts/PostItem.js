46d6ef70d56e52e7d9b8ffb22fdb0cda8ba479ca
Scott Ladd <scottjr101@gmail.com>
2020-02-10 22:42:39 -0500

    added style, got delete button working

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -13,19 +13,19 @@ const PostItem = ({
   post: { _id, text, name, avatar, user, likes, comments, date },
   showActions
 }) => (
-  <div className='card light-blue lighten-4 my-3 p-3'>
+  <div className='card lightBGcolor my-3 p-3'>
     <div className='d-flex justify-content-start'>
       <img
         className='avatar rounded-circle'
         src={`/uploads/${avatar}`}
-        alt='stock-pic'
+        alt=''
       />
-      <Link to={`/profile/${user}`}>
+      <Link to={`/profile/${user._id}`} className='text-warning'>
         <h4 className='ml-2'>{name}</h4>
       </Link>
     </div>
     <div>
-      <p className='mb-1 mt-2 p-2 border border-info roundedz'>{text}</p>
+      <p className='mb-1 mt-2 p-2 border border-dark sec roundedz'>{text}</p>
       <p className='font-weight-bold'>
         Posted on <Moment format='MM/DD/YYYY'>{date}</Moment>
       </p>
@@ -35,7 +35,7 @@ const PostItem = ({
           <button
             onClick={() => addLike(_id)}
             type='button'
-            className='btn btn-success btn-sm'
+            className='btn blue-gradient btn-sm'
           >
             <i className='fas fa-thumbs-up' />{' '}
             <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
@@ -43,17 +43,17 @@ const PostItem = ({
           <button
             onClick={() => removeLike(_id)}
             type='button'
-            className='btn btn-warning btn-sm'
+            className='btn purple-gradient btn-sm'
           >
             <i className='fas fa-thumbs-down' />
           </button>
-          <Link to={`/posts/${_id}`} className='btn btn-primary btn-sm'>
+          <Link to={`/posts/${_id}`} className='btn aqua-gradient btn-sm'>
             Discussion{' '}
             {comments.length > 0 && (
               <span className='badge badge-danger ml-1'>{comments.length}</span>
             )}
           </Link>
-          {!auth.loading && user === auth.user._id && (
+          {!auth.loading && user._id === auth.user._id && (
             <button
               onClick={() => deletePost(_id)}
               type='button'
